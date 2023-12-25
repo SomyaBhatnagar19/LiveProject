97d8e8c96d6b51778c94e50f7375959d63025e76
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-12-25 12:13:29 +0530

    Setup react-bootstrap installation.

---
+++ project1/frontend/src/App.js
@@ -0,0 +1,15 @@
+/* /frontend/App.js */
+import { Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <div>
+     Hello World
+     <Button variant="primary">
+      Click me
+    </Button>
+    </div>
+  );
+}
+
+export default App;
